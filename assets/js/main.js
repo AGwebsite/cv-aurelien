ab3863b86d5de6b843f3d717285fe13f975781d2
PenPen42 <gabertaurelien@gmail.com>
2021-03-15 14:07:57 +0100

    derniers changements

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,5 +123,5 @@ resumeButton.addEventListener('click', ()=>{
         //2. the PDF is generated
         generateResume()
         //3. The .scale-cv class is removed from the body after 5 seconds to return o normal size
-        setTimeout(removeScale, 5000)
+        //setTimeout(removeScale, 5000)
 })
